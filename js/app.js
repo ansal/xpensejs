126a2ca5fa3ced4b4fab3526236f23699a3e94ba
Ansal <ansal@bssatech.com>
2014-08-09 23:30:08 +0530

    added toolbar, panel and add popup

--- js/app.js
+++ js/app.js
@@ -10,12 +10,13 @@ var XpenseJS = XpenseJS || {};
   // shortcut for XpenseJS app variable
   var X = XpenseJS;
 
-  $(document).on('mobileinit', function(){
+  $(function(){
 
     // disable handlings of links and hashtagas by jquery mobile
     $.mobile.linkBindingEnabled = false;
     $.mobile.hashListeningEnabled = false;
-
+    $.mobile.pushStateEnabled = false;
+    
     // Fetch all collections
     X.Collections.Categories.fetch({ reset: true });
     X.Collections.Expenses.fetch({ reset: true });
